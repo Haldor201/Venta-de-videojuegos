3c7bb601d9fc5b3e2b16fa055f3bb7d4c9c24c9e
Haldor201 <calderonhaldor385@gmail.com>
2024-09-20 10:04:19 -0500

    secreto

--- server/db.js
+++ server/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
-
+/**
+ * Cuando GitHub Actions ejecuta el workflow, las variables definidas en secrets se 
+ * inyectan en process.env. No necesitas especificar nada más en tu código; simplemente
+ * process.env.MONGO_DB_URL tendrá el valor que configuraste en el secreto del repositorio.
+ */
 export const conectar=async ()=>{
     try {
-        await mongoose.connect('mongodb+srv://calderonhaldor385:Vp9Szac8E3nvDEWZ@so.de31nf8.mongodb.net/');   
+        await mongoose.connect(process.env.MONGO_DB_URL);   
         console.log("Conectado");
     } catch (error) {
         console.log(error);
